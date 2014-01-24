90fd00006bcf8fec7adfd16b730612c891cc1b26
Alston <alston@alston-VirtualBox.(none)>
2014-01-24 23:52:06 +0800

    Bug fixed

--- protocol.cpp
+++ protocol.cpp
@@ -21,7 +21,8 @@ uint32_t copy2msg(MYMSG* destMsg,uint16_t content,uint32_t msgPos,bool hasLength
         memcpy(&destMsg[msgPos],&nlength,4);
         msgPos+=4;
     }
-    memcpy(&destMsg[msgPos],&content,2);
+    uint16_t nContent=htons(content);
+    memcpy(&destMsg[msgPos],&nContent,2);
     msgPos+=2;
     return msgPos;
 }
@@ -33,27 +34,19 @@ uint32_t copy2msg(MYMSG* destMsg,uint32_t content,uint32_t msgPos,bool hasLength
         memcpy(&destMsg[msgPos],&nlength,4);
         msgPos+=4;
     }
-    memcpy(&destMsg[msgPos],&content,4);
+    uint32_t nContent=htonl(content);
+    memcpy(&destMsg[msgPos],&nContent,4);
     msgPos+=4;
     return msgPos;
 }
 
-//Bug free
 ERRCOD createLoginMsg(MYMSG* destMsg,char* userName, uint16_t portNum)
 {
-    uint16_t posMsg=5;
+    uint16_t posMsg=1;
     destMsg[0]=LOGIN;
     uint32_t length=strlen(userName);
-    uint32_t nlength=htonl(length);
-    memcpy(&destMsg[1],&nlength,4);
-    memcpy(&destMsg[5],userName,length);
-    posMsg+=length;
-    length=2;
-    nlength=htonl(length);
-    memcpy(&destMsg[posMsg],&nlength,4);
-    posMsg+=4;
-    uint16_t nportNum=htons(portNum);
-    memcpy(&destMsg[posMsg],&nportNum,2);
+    posMsg=copy2msg(destMsg,length,(uint8_t*)userName,posMsg);
+    posMsg=copy2msg(destMsg,portNum,posMsg,true);
     return 0;
 }
 
@@ -90,7 +83,7 @@ ERRCOD clientList::addUser(MYMSG* sourMsg, uint32_t ipAddr)
     newUser->nameLength=length;
     memcpy(&(newUser->name),&sourMsg[5],length);
     newUser->name[length+1]=0;
-    posMsg+=(length+2);
+    posMsg+=(length+4);
     uint16_t nPortNum;
     memcpy(&nPortNum,&sourMsg[posMsg],2);
     uint16_t portNum=ntohs(nPortNum);
@@ -99,7 +92,6 @@ ERRCOD clientList::addUser(MYMSG* sourMsg, uint32_t ipAddr)
 
     if (start==NULL)
     {
-        userNum++;
         start=newUser;
     }
     else
@@ -123,6 +115,7 @@ ERRCOD clientList::addUser(MYMSG* sourMsg, uint32_t ipAddr)
         }
         last->next=newUser;
     }
+    userNum++;
     return 0;
 }
 
@@ -137,7 +130,7 @@ ERRCOD clientList::delUser(char delName[])
         {
             if (last==NULL)
             {
-                start=NULL;
+                start=cur->next;
                 delete cur;
                 find=true;
             }
@@ -151,7 +144,11 @@ ERRCOD clientList::delUser(char delName[])
         last=cur;
         cur=cur->next;
     }
-    if (find) return 0;
+    if (find)
+    {
+        userNum--;
+        return 0;
+    }
     else return NOSUCHUSER;
 }
 
@@ -161,13 +158,14 @@ ERRCOD clientList::getList(MYMSG* destMsg)
     uint32_t totalByte=0;
     destMsg[0]=GET_LIST_OK;
     userInf* cur=start;
-    while(start!=NULL)
+    while(cur!=NULL)
     {
         uint32_t length=cur->nameLength;
         msgPos=copy2msg(destMsg,length,(uint8_t*)cur->name,msgPos);
         msgPos=copy2msg(destMsg,cur->ipAddr,msgPos,false);
         msgPos=copy2msg(destMsg,cur->portNum,msgPos,false);
         totalByte+=(length+10);
+        cur=cur->next;
     }
     copy2msg(destMsg,totalByte,1,false);
     return 0;
@@ -175,6 +173,7 @@ ERRCOD clientList::getList(MYMSG* destMsg)
 
 clientList::clientList()
 {
+    userNum=0;
     start=NULL;
 }
 
@@ -189,7 +188,6 @@ clientList::~clientList()
         delete last;
     }
 }
-
 
 userInf::userInf()
 {
--- server.cpp
+++ server.cpp
@@ -1,10 +1,7 @@
 #include <stdio.h>
+#include <arpa/inet.h>
 #include "protocol.h"
 int main(void)
 {
-    MYMSG msg[MSGL];
-    char name[512];
-    scanf("%s",name);
-    createLoginMsg(msg,name,12345);
     return 0;
 }
